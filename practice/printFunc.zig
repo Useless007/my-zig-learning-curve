62fcd829167e1c2865494a8aeab2c4e39422dc55
Useless007 <mammoth17216@gmail.com>
2025-03-21 00:04:57 +0700

    Update README.md and printFunc.zig with additional examples and formatting improvements

--- practice/printFunc.zig
+++ practice/printFunc.zig
@@ -2,15 +2,19 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() void {
-    const name = "Jintaphas";
+    const name = "Jintaphas"; //Zig ไม่มีชนิดข้อมูล String แบบที่มีในภาษาอื่นๆ
     const age = 23;
     const height = 175;
     const weight = 78.9;
     const is_student = true;
 
+    printInfo(name, age, height, weight, is_student);
+}
+
+fn printInfo(name: []const u8, age: i16, height: f32, weight: f32, is_student: bool) void {
     print("Hello {s}\n", .{name});
-    print("Age: {d}\n",.{age});
-    print("Height: {}\n",.{height});
-    print("Weight: {d:.2}\n",.{weight});
+    print("Age: {d}\n", .{age});
+    print("Height: {}\n", .{height});
+    print("Weight: {d:.2}\n", .{weight});
     print("Is student: {}\n", .{is_student});
 }
